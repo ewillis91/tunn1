1b7ea1b9b6f759be3de7972370a6c6600ba7ea5f
ewillis91 <emmawillis983@gmail.com>
2023-09-14 15:41:44 +0100

    Navigate to WelcomeScreen if SignUp successful

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -20,7 +20,7 @@ const LoginScreen: React.FC = () => {
         });
       console.log('Sign in successful with ' + email);
       // @ts-ignore
-      navigation.navigate('SpotifyLogin');
+      navigation.navigate('WelcomeScreen');
     } catch (error: any) {
       console.log('error signing in', error);
       if (error.code === 'NotAuthorizedException' || error.code === 'UserNotFoundException' ) {
