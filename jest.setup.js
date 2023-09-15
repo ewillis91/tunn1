8a0e701bd02611b9c1c467f4116c6111e08e38e5
ewillis91 <emmawillis983@gmail.com>
2023-09-15 10:54:48 +0100

    Nuke Jest set up to retry all over again

--- jest.setup.js
+++
@@ -1,8 +0,0 @@
-import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
-
-jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
-jest.mock('@react-native-community/netinfo', () => ({
-    addEventListener: jest.fn(),
-    fetch: jest.fn().mockResolvedValue({ isConnected: true }), 
-  }));
-  
